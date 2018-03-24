a1c68328c5115c641fe305b979cf1560b1078fb9
Walter Cheng <walterpakwai@live.com>
2018-03-24 02:11:03 -0400

    adding generic control for players

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -9,10 +9,22 @@ public class PlayerControl : MonoBehaviour {
     public bool jump = false;
     [HideInInspector]
     public bool doubleJump = false;
+    [HideInInspector]
+    public bool leftClick = false;
+    [HideInInspector]
+    public bool rightClick = false;
+
+    //button define for this player
+    public KeyCode jumpButton;
+    public KeyCode leftButton;
+    public KeyCode rightButton;
+    public KeyCode shootButton;
 
     //user define player values
+    public Rigidbody2D rb2d;
     public float speed;
     public float jumpForce;
+    public bool faceLeft;
 
     //user define bullet values
     public float bulletSpeed;
@@ -22,29 +34,22 @@ public class PlayerControl : MonoBehaviour {
     //user define / future triggerable
     public bool canDoubleJump;
 
-    private Rigidbody2D rb2d;
-
-    private Transform groundCheck;
     private bool grounded = false;
 
-    void Awake()
-    {
-        groundCheck = transform.Find("groundCheck");
-    }
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        //rb2d = GetComponent<Rigidbody2D>();
         rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(shootButton))
         {
             Fire();
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(jumpButton))
         {
             if (grounded)
             {
@@ -55,21 +60,22 @@ public class PlayerControl : MonoBehaviour {
                 jump = true;
                 doubleJump = false;
             }
-        }  
+        }
+
+        if (Input.GetKeyDown(leftButton)) leftClick = true;
+        if (Input.GetKeyUp(leftButton)) leftClick = false;
+        if (Input.GetKeyDown(rightButton)) rightClick = true;
+        if (Input.GetKeyUp(rightButton)) rightClick = false;
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = 0;
+        if (leftClick) moveHorizontal--;
+        if (rightClick) moveHorizontal++;
+
         Vector2 movement = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
         rb2d.velocity = movement;
-        /*float movejump;
-        if (moveVertical <= 0)
-            movejump = 0;
-        else
-            movejump = moveVertical * jumpForce;
-        rb2d.AddForce(new Vector2(0, movejump));*/
 
         if (jump)
         {
@@ -104,7 +110,13 @@ public class PlayerControl : MonoBehaviour {
             bulletSpawn.position,
             bulletSpawn.rotation);
 
-        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+        float bulletVelocity;
+        if (faceLeft)
+            bulletVelocity = -bulletSpeed;
+        else
+            bulletVelocity = bulletSpeed;
+
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletVelocity, 0);
 
         Destroy(bullet, 2.0f);
     }
