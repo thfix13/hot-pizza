e3a9ef246b5471c9679ece6b5da072bf95ed8eed
Walter Cheng <walterpakwai@live.com>
2018-03-23 22:29:24 -0400

    first upload

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+    public bool fixedSpeed;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if (fixedSpeed)
+            rb2d.velocity = movement * speed;
+        if (!fixedSpeed)
+            rb2d.AddForce(movement * speed);
+    }
+}
