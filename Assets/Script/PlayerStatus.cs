4f2ff293f7e292e96e8d69de9869290c820d3bca
Walter Cheng <walterpakwai@live.com>
2018-03-24 10:49:55 -0400

    adding full health

--- Assets/Script/PlayerStatus.cs
+++ Assets/Script/PlayerStatus.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour {
 
     public int life;
+    public float fullhealth;
     public float health;
     public float bullet_damage;
     public float bullet_speed;
